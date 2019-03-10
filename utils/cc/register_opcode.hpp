fb425ad53aa72462bde8462c0829a8f7233dc547
stryku <stryku2393@gmail.com>
2019-03-10 20:23:42 +0100

    Introduced register opcodes

---
+++ utils/cc/register_opcode.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+namespace esvm::cc
+{
+    enum class register_opcode
+    {
+        rret,
+        rip,
+        rrem,
+        r0,
+        r1,
+        r2,
+        r3,
+        r4,
+        r5,
+        r6,
+        r7,
+        r8,
+        r9,
+        r10,
+        r11,
+        r12,
+        r13,
+        r14,
+        r15
+    };
+}
